5e46eb91cae391c494dcda7e1c37e823daec0197
maiyo008 <tonymaiyo008@gmail.com>
2023-04-23 12:13:50 +0000

    List state objects

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" a script that starts a Flask web application"""
+from models import storage
+from models.state import State
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """Close current SQLAlchemy sessions"""
+    storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def list_states():
+    """List all state objects present in DBStorage"""
+    states = sorted(storage.all(State).values(), key=lambda state: state.name)
+    return render_template('7-states_list.html', states=states)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
