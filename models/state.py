ddd1926af6a2f7d437d8ce7cf3a3428b49d67d00
maiyo008 <tonymaiyo008@gmail.com>
2023-04-26 12:05:31 +0000

    Update State class constructor and property getter for cities in state

--- models/state.py
+++ models/state.py
@@ -7,19 +7,22 @@ from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
-
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    cities = relationship('City', backref='state', cascade="all, delete") 
+    cities = relationship('City', backref='state', cascade="all, delete")
+
+    def __init__(self, *args, **kwargs):
+        """Instantiates a new State model"""
+        super().__init__(*args, **kwargs)
 
     @property
     def cities(self):
         from models import storage
         related_cities = []
 
-        cities = storage.all(City).items()
+        cities = storage.all(City)
         for city in cities.values():
             if city.state_id == self.id:
                 related_cities.append(city)
