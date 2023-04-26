5b0c976ff9db8de121436a674b0f5c58e2ea1ea8
maiyo008 <tonymaiyo008@gmail.com>
2023-04-26 22:24:06 +0000

    Add collation for amenity_id

--- models/place.py
+++ models/place.py
@@ -26,7 +26,7 @@ class Place(BaseModel, Base):
 
     place_amenity = Table('place_amenity', Base.metadata,
         Column('place_id', String(60), ForeignKey('places.id'), primary_key=True, nullable=False),
-        Column('amenity_id', String(60), ForeignKey('amenities.id'), primary_key=True, nullable=False)
+        Column('amenity_id', String(60, collation='latin1_swedish_ci'), ForeignKey('amenities.id'), primary_key=True, nullable=False)
     )
 
     @property
