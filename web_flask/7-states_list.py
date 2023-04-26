87574fe1f2c2bb772643e4a3f64f4d132c95fe6e
maiyo008 <tonymaiyo008@gmail.com>
2023-04-26 14:54:57 +0000

    Modify script to only import models when run as main

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 """ a script that starts a Flask web application"""
-from models import storage
-from models.state import State
 from flask import Flask
 from flask import render_template
 app = Flask(__name__)
@@ -21,4 +19,6 @@ def list_states():
 
 
 if __name__ == '__main__':
+    from models import storage
+    from models.state import State
     app.run(host='0.0.0.0', port=5000)
